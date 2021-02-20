bd0ae378e47f9c414402d5523b76df40b0faa070
kelly-x <78989837+kelly-x@users.noreply.github.com>
2021-02-20 16:16:14 -0500

    first commit

---
+++ config/index.js
@@ -0,0 +1,103 @@
+module.exports = {
+  //-- SITE SETTINGS -----
+  author: "@kellyx",
+  siteTitle: "Kelly's Personal Portfolio",
+  siteShortTitle: "Kelly Xu", // Used as logo text in header, footer, and splash screen
+  siteDescription:
+  "All the world is a laboratory to the inquiring mind.",
+  siteUrl: "https://github.com/kelly-x/kelly-xu-portfolio.git",
+  siteLanguage: "en_US",
+  siteIcon: "content/favicon.png", // Relative to gatsby-config file
+  seoTitleSuffix: "Kelly's Personal Portfolio", // SEO title syntax will be e.g. "Imprint - {seoTitleSuffix}"
+
+  // -- THEME SETTINGS -----
+  colors: {
+    lightTheme: {
+      primary: "#000000",
+      secondary: "#FFF4D9",
+      tertiary: "#F2F2F2",
+      text: "#000000",
+      subtext: "#555555",
+      background: "#FFFFFF",
+      card: "#FFFFFF",
+      scrollBar: "rgba(0, 0, 0, 0.5)",
+      boxShadow: "rgba(0, 0, 0, 0.16)",
+      boxShadowHover: "rgba(0, 0, 0, 0.32)",
+    },
+    darkTheme: {
+      primary: "#FAFAFA",
+      secondary: "#2A2926",
+      tertiary: "#252525",
+      text: "rgba(255, 255, 255, 0.87)",
+      subtext: "#AAAAAA",
+      background: "#121212",
+      card: "#1C1C1C",
+      scrollBar: "rgba(255, 255, 255, 0.5)",
+      boxShadow: "rgba(0, 0, 0, 0.16)",
+      boxShadowHover: "rgba(0, 0, 0, 0.32)",
+    },
+  },
+  fonts: {
+    primary: "Roboto, Arial, sans-serif",
+  },
+
+  //-- ARTICLES SECTION SETTINGS -----
+  // You can create your own Medium feed with this rss to json converter: https://rss2json.com/
+  // To access your Medium RSS feed, just replace this url with your username: https://medium.com/feed/@{yourname}
+  // mediumRssFeed:
+    // "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40konstantinmuenster",
+  // rssFeed: "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.theguardian.com%2Finternational%2Frss",
+
+   shownArticles: 0,
+
+  //-- SOCIAL MEDIA SETTINGS -----
+  // There are icons available for the following platforms:
+  // Medium, GitHub, LinkedIn, XING, Behance
+  socialMedia: [
+    {
+      name: "Linkedin",
+      url: "https://www.linkedin.com/in/kelly-xu-/",
+    },
+    {
+      name: "Medium",
+      url: "https://medium.com/@kelly.cy.xu",
+    },
+    {
+      name: "Resume",
+      url:
+        "https://drive.google.com/file/d/1A7Rl2IPuK45QcuJQUWSKXhUxChqngWw3/view?usp=sharing",
+    },
+  ],
+
+  //-- NAVIGATION SETTINGS -----
+  navLinks: {
+    menu: [
+      // {
+      //   name: "Articles",
+      //   url: "/#articles",
+      // },
+      {
+        name: "About Me",
+        url: "/#about",
+      },
+      {
+        name: "Projects",
+        url: "/#projects",
+      },
+    ],
+    button: {
+      name: "Contact",
+      url: "/#contact",
+    },
+  },
+  footerLinks: [
+    {
+      name: "Privacy",
+      url: "/privacy",
+    },
+    {
+      name: "Imprint",
+      url: "/imprint",
+    },
+  ],
+}
